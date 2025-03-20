ae0e3025868b07a4868a9333f976f276da8f7625
zdan <ndholovu@gmail.com>
2025-03-20 12:55:25 +0900

    データベース連携

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
-from flask import Flask,render_template,request
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-app=Flask(__name__)
-app.config['SECRET_KEY']= 'mysecretkey'
 
+app = Flask(__name__)
 
-from forms import UserInfoForm
+# 環境変数DATABASE_URLを使ってDBに接続する設定
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///local.db')
+db = SQLAlchemy(app)
+
+# 例のモデル
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    users = User.query.all()
+    return ', '.join([user.name for user in users])
 
-@app.route('/form',methods=['GET','POST'])
-def show_enter():
-    form=UserInfoForm(request.form)
-    
-    if request.method=='POST' and form.validate():
-        return render_template('result.html',form=form)
-
-    return render_template('form.html',form=form)
+migrate=Migrate(app,db)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
