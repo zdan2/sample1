8b3ce93fe95748b4cda0aa55fcf63a5fb64cb752
zdan <ndholovu@gmail.com>
2025-03-20 14:51:35 +0900

    db3

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 
@@ -14,6 +16,9 @@ migrate = Migrate(app, db)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
+    
+admin = Admin(app, name='管理画面', template_mode='bootstrap3')
+admin.add_view(ModelView(User, db.session))
 
 # トップページ
 @app.route('/')
