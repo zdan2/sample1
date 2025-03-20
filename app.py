730c043258dfc78fde814ef6d2eaea3e044a6f60
zdan <ndholovu@gmail.com>
2025-03-20 14:41:50 +0900

    db2

--- app.py
+++ app.py
@@ -3,24 +3,25 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-
 app = Flask(__name__)
 
-# 環境変数DATABASE_URLを使ってDBに接続する設定
+# DB接続設定
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///local.db')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-# 例のモデル
+# Userモデルの定義
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
 
+# トップページ
 @app.route('/')
 def index():
     users = User.query.all()
-    return ', '.join([user.name for user in users])
-
-migrate=Migrate(app,db)
+    if not users:
+        return 'ユーザーが登録されていません'
+    return ', '.join(user.name for user in users)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
